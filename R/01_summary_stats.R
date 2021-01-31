61d767667156357b2373e76134918d1baa90268f
Mark Scheuerell <scheuerl@uw.edu>
2021-01-31 05:49:23 -0800

    added pisaster data

---
+++ R/01_summary_stats.R
@@ -0,0 +1,17 @@
+## this script loads the data and calculates some summary statistics
+
+## load libraries
+library("here")
+
+## set location of the data directory 
+data_dir <- here("data")
+
+## load data file
+readRDS(file.path(data_dir, "pisaster_data.Rds"))
+
+## peek at the data
+head(pisaster_data)
+
+## calculate mean counts across all years, sites, and plots
+mean_count <- mean(pisaster_data$count)
+
